89376e78fc5c047997ff844f2e163cafac7517bb
Paul <gigi.boss52@gmail.com>
2021-12-05 14:29:33 +0200

    Rearrange interface + finish error checking

--- least-squares.py
+++ least-squares.py
@@ -52,21 +52,31 @@ def create_plot(xs, ys):
     plt.xlabel("x")
     plt.ylabel("y")
     plt.plot(xs, ys, color='red', label='Function')
-    plt.show()
 
 
 def create_line():
+    hide_error()
     global entries
     data = entries.entries
+    if not entries.check_values():
+        print_error("Invalid values")
+        return
     x = calculate(data)
-    m, b = x
+    if not x:
+        print_error("Invalid data")
+        return
+    else:
+        m, b = x
     func = make_function(m, b)
     xs, ys = parse_points(func, data)
+    plt.title("Function: (" + str(m) + ") * x + (" + str(b) + ")")
     create_plot(xs, ys)
+    plt.show()
 
 
 class Entries:
     entries = dict()
+    start = 1
     length = 0
 
     def add(self):
@@ -74,8 +84,8 @@ class Entries:
         entry2 = ttk.Entry(frm)
         self.entries.update({self.length: {"x": entry1, "y": entry2}})
         self.length += 1
-        entry1.grid(column=0, row=self.length)
-        entry2.grid(column=1, row=self.length)
+        entry1.grid(column=0, row=self.length + self.start)
+        entry2.grid(column=1, row=self.length + self.start)
 
     def check_values(self):
         try:
@@ -90,13 +100,12 @@ root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 error = ttk.Label(frm)
-ttk.Label(frm, text="X").grid(column=0, row=0)
-ttk.Label(frm, text="Y").grid(column=1, row=0)
-ttk.Button(frm, text="Calculate", command=create_line).grid(column=2, row=0)
+ttk.Button(frm, text="Calculate", command=create_line).grid(column=1, row=0)
+ttk.Label(frm, text="X").grid(column=0, row=1)
+ttk.Label(frm, text="Y").grid(column=1, row=1)
 entries = Entries()
 entries.add()
 entries.add()
-ttk.Button(frm, text="Add", command=entries.add).grid(column=2, row=1)
-
+ttk.Button(frm, text="Add", command=entries.add).grid(column=2, row=2)
 
 root.mainloop()
