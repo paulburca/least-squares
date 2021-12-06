ef5e95b1830f8425f4e7d5fde8842897ff3b28b9
Paul <gigi.boss52@gmail.com>
2021-12-06 15:08:24 +0200

    error checks, remove entries button, added data to plot

--- least-squares.py
+++ least-squares.py
@@ -54,6 +54,13 @@ def create_plot(xs, ys, yfs):
     plt.ylabel("y")
     plt.plot(xs, yfs, color='red', label='Function')
     plt.scatter(xs, ys)
+    labels = [f"({x},{y})" for x, y in zip(xs, ys)]
+    for label, x, y in zip(labels, xs, ys):
+        plt.annotate(label, xy=(x, y), xytext=(5, -5), textcoords='offset points')
+    for x, y, yf in zip(xs, ys, yfs):
+        label = str(abs(y - yf))
+        plt.plot([x, x], [y, yf], color='black', linestyle='dashed', label=label)
+    plt.plot(xs, ys, color="blue")
     mx = max(max(xs), max(ys), max(yfs))
     mn = min(min(xs), min(ys), min(yfs))
     plt.xlim([mn - 0.5, mx + 0.5])
@@ -75,6 +82,7 @@ def create_line():
         m, b = x
     func = make_function(m, b)
     xs, ys, yfs = parse_points(func, data)
+    plt.clf()
     plt.title("Function: (" + str(m) + ") * x + (" + str(b) + ")")
     create_plot(xs, ys, yfs)
     plt.show()
@@ -86,12 +94,28 @@ class Entries:
     length = 0
 
     def add(self):
-        entry1 = ttk.Entry(frm)
-        entry2 = ttk.Entry(frm)
-        self.entries.update({self.length: {"x": entry1, "y": entry2}})
-        self.length += 1
-        entry1.grid(column=0, row=self.length + self.start)
-        entry2.grid(column=1, row=self.length + self.start)
+        hide_error()
+        if self.length != 35:
+            entry1 = ttk.Entry(frm)
+            entry2 = ttk.Entry(frm)
+            self.entries.update({self.length: {"x": entry1, "y": entry2}})
+            self.length += 1
+            entry1.grid(column=0, row=self.length + self.start)
+            entry2.grid(column=1, row=self.length + self.start)
+        else:
+            print_error("Maximum set\nof values: 35")
+
+    def remove(self):
+        hide_error()
+        if self.length != 2:
+            self.length -= 1
+            entryx = self.entries.get(self.length).get("x")
+            entryy = self.entries.get(self.length).get("y")
+            self.entries.pop(self.length)
+            entryx.destroy()
+            entryy.destroy()
+        else:
+            print_error("Minimum set\nof values: 2")
 
     def check_values(self):
         try:
@@ -113,5 +137,6 @@ entries = Entries()
 entries.add()
 entries.add()
 ttk.Button(frm, text="Add", command=entries.add).grid(column=2, row=2)
+ttk.Button(frm, text="Remove", command=entries.remove).grid(column=2, row=3)
 
 root.mainloop()
