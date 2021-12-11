d37d10a85e173fc1d4859bde0b21247aa8276667
Paul <gigi.boss52@gmail.com>
2021-12-11 14:50:31 +0200

    Created class for interface
    Created a class in which we build the interface and keep the data related to it organised. I added the hide and print error functions inside it and I split the interface building steps in functions.

--- least-squares.py
+++ least-squares.py
@@ -10,17 +10,6 @@ def make_function(m, b):
     return func
 
 
-def print_error(data):
-    global error
-    error.config(text=data)
-    error.grid(column=2, row=0)
-
-
-def hide_error():
-    global error
-    error.grid_forget()
-
-
 def calculate(data):
     sumx = 0
     sumy = 0
@@ -72,7 +61,7 @@ def create_plot(xs, ys, yfs):
 def create_line(data):
     x = calculate(data)
     if not x:
-        print_error("Invalid data")
+        interface.print_error("Invalid data")
         return
     else:
         m, b = x
@@ -105,38 +94,37 @@ def parse_data(data):
 
 
 def execute_file():
-    global f
+    f = interface.get_file_name()
     if f:
         with open(f, 'r') as t:
             txt = t.read()
         values = parse_data(txt)
         if not values:
-            print_error("Invalid values")
+            interface.print_error("Invalid values")
         else:
             create_line(values)
     else:
-        print_error("File not\nselected")
+        interface.print_error("File not\nselected")
 
 
 def execute_textbox():
-    global text
-    txt = text.get("1.0", "end")
+    txt = interface.get_text().get("1.0", "end")
     if len(txt) == 1:
-        print_error("No data in\nthe textbox")
+        interface.print_error("No data in\nthe textbox")
         return
     values = parse_data(txt)
     if not values:
-        print_error("Invalid values")
+        interface.print_error("Invalid values")
     else:
         create_line(values)
 
 
 def execute_entries():
-    global entries
+    entries = interface.get_entries()
     data = dict()
     i = 0
     if not entries.check_values():
-        print_error("Invalid values")
+        interface.print_error("Invalid values")
         return
     for entry in entries.entries.values():
         data.update({i: {"x": float(entry.get("x").get()), "y": float(entry.get("y").get())}})
@@ -145,11 +133,10 @@ def execute_entries():
 
 
 def execute():
-    global var
-    hide_error()
-    val = var.get()
+    interface.hide_error()
+    val = interface.get_var().get()
     if val == 0:
-        print_error("Method not\nselected")
+        interface.print_error("Method not\nselected")
     elif val == 1:
         execute_file()
     elif val == 2:
@@ -163,20 +150,23 @@ class Entries:
     start = 4
     length = 0
 
+    def __init__(self, i):
+        self.interface = i
+
     def add(self):
-        hide_error()
+        self.interface.hide_error()
         if self.length != 35:
-            entry1 = ttk.Entry(frm)
-            entry2 = ttk.Entry(frm)
+            entry1 = ttk.Entry(self.interface.get_frame())
+            entry2 = ttk.Entry(self.interface.get_frame())
             self.entries.update({self.length: {"x": entry1, "y": entry2}})
             self.length += 1
             entry1.grid(column=0, row=self.length + self.start)
             entry2.grid(column=1, row=self.length + self.start)
         else:
-            print_error("Maximum set\nof values: 35")
+            self.interface.print_error("Maximum set\nof values: 35")
 
     def remove(self):
-        hide_error()
+        self.interface.hide_error()
         if self.length != 2:
             self.length -= 1
             entryx = self.entries.get(self.length).get("x")
@@ -185,7 +175,7 @@ class Entries:
             entryx.destroy()
             entryy.destroy()
         else:
-            print_error("Minimum set\nof values: 2")
+            self.interface.print_error("Minimum set\nof values: 2")
 
     def check_values(self):
         try:
@@ -201,33 +191,101 @@ def openfile():
         ('text files', '*.txt'),
         ('All files', '*.*')
     )
-    global f, file_label
-    f = fd.askopenfilename(filetypes=filetypes)
+    file_label = interface.get_file_label()
+    interface.get_file_label().grid_forget()
+    interface.set_file_name(fd.askopenfilename(filetypes=filetypes))
+    if not interface.get_file_name():
+        file_label.config(text="File not selected")
     file_label.grid(column=1, row=3)
 
 
-f = ''
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-error = ttk.Label(frm)
-ttk.Button(frm, text="Calculate", command=execute).grid(column=1, row=0)
-var = tk.IntVar()
-checkboxes = list()
-checkboxes.append(ttk.Checkbutton(frm, text="File", onvalue=1, variable=var).grid(row=1, column=0))
-checkboxes.append(ttk.Checkbutton(frm, text="TextBox", onvalue=2, variable=var).grid(row=1, column=1))
-checkboxes.append(ttk.Checkbutton(frm, text="DataList", onvalue=3, variable=var).grid(row=1, column=2))
-ttk.Button(frm, text="Upload file", command=openfile).grid(column=0, row=3)
-file_label = ttk.Label(frm, text="File uploaded")
-ttk.Label(frm, text="Text:").grid(column=0, row=2)
-text = tk.Text(frm, height=5, width=25)
-text.grid(columnspan=2, column=1, row=2)
-ttk.Label(frm, text="X").grid(column=0, row=4)
-ttk.Label(frm, text="Y").grid(column=1, row=4)
-entries = Entries()
-entries.add()
-entries.add()
-ttk.Button(frm, text="Add", command=entries.add).grid(column=2, row=5)
-ttk.Button(frm, text="Remove", command=entries.remove).grid(column=2, row=6)
+class Interface:
+    def __init__(self):
+        def build_frame():
+            root = Tk()
+            frm = ttk.Frame(root, padding=10)
+            frm.grid()
+            return root, frm
 
-root.mainloop()
+        def build_button(frm):
+            ttk.Button(frm, text="Calculate", command=execute).grid(column=1, row=0)
+
+        def build_error(frm):
+            error = ttk.Label(frm)
+            return error
+
+        def build_checkboxes(frm):
+            var = tk.IntVar()
+            checkboxes = list()
+            checkboxes.append(ttk.Checkbutton(frm, text="File", onvalue=1, variable=var).grid(row=1, column=0))
+            checkboxes.append(ttk.Checkbutton(frm, text="TextBox", onvalue=2, variable=var).grid(row=1, column=1))
+            checkboxes.append(ttk.Checkbutton(frm, text="DataList", onvalue=3, variable=var).grid(row=1, column=2))
+            return var
+
+        def build_file(frm):
+            ttk.Button(frm, text="Upload file", command=openfile).grid(column=0, row=3)
+            file_label = ttk.Label(frm, text="File uploaded")
+            return file_label
+
+        def build_text(frm):
+            ttk.Label(frm, text="Text:").grid(column=0, row=2)
+            text = tk.Text(frm, height=5, width=25)
+            text.grid(columnspan=2, column=1, row=2)
+            return text
+
+        def build_entries(frm):
+            entries = Entries(self)
+            ttk.Label(frm, text="X").grid(column=0, row=4)
+            ttk.Label(frm, text="Y").grid(column=1, row=4)
+            entries.add()
+            entries.add()
+            ttk.Button(frm, text="Add", command=entries.add).grid(column=2, row=5)
+            ttk.Button(frm, text="Remove", command=entries.remove).grid(column=2, row=6)
+            return entries
+
+        self.__root, self.__frm = build_frame()
+        build_button(self.__frm)
+        self.__error = build_error(self.__frm)
+        self.__var = build_checkboxes(self.__frm)
+        self.__file_label = build_file(self.__frm)
+        self.__file_name = ''
+        self.__text = build_text(self.__frm)
+        self.__entries = build_entries(self.__frm)
+
+    def get_frame(self):
+        return self.__frm
+
+    def get_error(self):
+        return self.__error
+
+    def get_var(self):
+        return self.__var
+
+    def get_file_label(self):
+        return self.__file_label
+
+    def get_file_name(self):
+        return self.__file_name
+
+    def set_file_name(self, file_name):
+        self.__file_name = file_name
+
+    def get_text(self):
+        return self.__text
+
+    def get_entries(self):
+        return self.__entries
+
+    def print_error(self, data):
+        self.__error.config(text=data)
+        interface.get_error().grid(column=2, row=0)
+
+    def hide_error(self):
+        self.__error.grid_forget()
+
+    def show(self):
+        self.__root.mainloop()
+
+
+interface = Interface()
+interface.show()
