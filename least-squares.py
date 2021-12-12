27ff23688603f00bf0a1be5a5e1449247c404a55
Paul <gigi.boss52@gmail.com>
2021-12-12 13:31:45 +0200

    Started putting data into classes

--- least-squares.py
+++ least-squares.py
@@ -3,159 +3,167 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog as fd
 import matplotlib.pyplot as plt
+from abc import ABC, abstractmethod
 
 
-def make_function(m, b):
-    def func(x): return m * x + b
-    return func
+class Command:
+    @abstractmethod
+    def run(self):
+        pass
 
-
-def calculate(data):
-    sumx = 0
-    sumy = 0
-    sumx2 = 0
-    sumxy = 0
-
-    points = data
-    for entry in points.values():
-        x = float(entry.get("x"))
-        y = float(entry.get("y"))
-        sumx += x
-        sumy += y
-        sumx2 += x * x
-        sumxy += x * y
-    n = len(points)
-    try:
-        m = (n * sumxy - sumx * sumy) / (n * sumx2 - sumx * sumx)
-        b = (sumy - m * sumx) / n
-    except ZeroDivisionError:
-        return False
-    return m, b
-
-
-def parse_points(func, data):
-    xs = [float(i.get("x")) for i in data.values()]
-    ys = [float(i.get("y")) for i in data.values()]
-    yfs = [func(x) for x in xs]
-    return xs, ys, yfs
-
-
-def create_plot(xs, ys, yfs):
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.plot(xs, yfs, color='red', label='Function')
-    plt.scatter(xs, ys)
-    labels = [f"({x},{y})" for x, y in zip(xs, ys)]
-    for label, x, y in zip(labels, xs, ys):
-        plt.annotate(label, xy=(x, y), xytext=(5, -5), textcoords='offset points')
-    for x, y, yf in zip(xs, ys, yfs):
-        label = str(abs(y - yf))
-        plt.plot([x, x], [y, yf], color='black', linestyle='dashed', label=label)
-    plt.plot(xs, ys, color="blue")
-    mx = max(max(xs), max(ys), max(yfs))
-    mn = min(min(xs), min(ys), min(yfs))
-    plt.xlim([mn - 0.5, mx + 0.5])
-    plt.ylim([mn - 0.5, mx + 0.5])
-
-
-def create_line(data):
-    x = calculate(data)
-    if not x:
-        interface.print_error("Invalid data")
-        return
-    else:
-        m, b = x
-    func = make_function(m, b)
-    xs, ys, yfs = parse_points(func, data)
-    plt.clf()
-    plt.title("Function: (" + str(m) + ") * x + (" + str(b) + ")")
-    create_plot(xs, ys, yfs)
-    plt.show()
-
-
-def parse_data(data):
-    pairs = dict()
-    temp = data.replace('(', '').split('),')
-    i = 0
-    if len(temp) == 1:
-        return False
-    for t in temp:
-        vals = t.replace(')', '').replace(' ', '').split(',')
-        if len(vals) < 2:
+    @staticmethod
+    def parse_data(data):
+        pairs = dict()
+        temp = data.replace('(', '').split('),')
+        i = 0
+        if len(temp) == 1:
             return False
-        pairs.update({i: {"x": vals[0], "y": vals[1]}})
-        i += 1
-        try:
-            float(pairs.get(i - 1).get('x'))
-            float(pairs.get(i - 1).get('y'))
-        except ValueError:
-            return False
-    return pairs
+        for t in temp:
+            vals = t.replace(')', '').replace(' ', '').split(',')
+            if len(vals) < 2:
+                return False
+            pairs.update({i: {"x": vals[0], "y": vals[1]}})
+            i += 1
+            try:
+                float(pairs.get(i - 1).get('x'))
+                float(pairs.get(i - 1).get('y'))
+            except ValueError:
+                return False
+        return pairs
 
 
-def openfile():
-    filetypes = (
-        ('text files', '*.txt'),
-        ('All files', '*.*')
-    )
-    file_label = interface.get_file_label()
-    interface.get_file_label().grid_forget()
-    interface.set_file_name(fd.askopenfilename(filetypes=filetypes))
-    if not interface.get_file_name():
-        file_label.config(text="File not selected")
-    file_label.grid(column=1, row=3)
+class ExecuteCommandHandler:
+    def __init__(self):
+        self.__cmds = dict()
+        self.__cmds[0] = interface.print_error
+        self.__cmds[1] = ExecuteFileCommand().run
+        self.__cmds[2] = ExecuteTextCommand().run
+        self.__cmds[3] = ExecuteDataCommand().run
+        interface.hide_error()
+
+    def execute(self):
+        self.__cmds[interface.get_var().get()]()
 
 
-def execute_file():
-    f = interface.get_file_name()
-    if f:
-        with open(f, 'r') as t:
-            txt = t.read()
-        values = parse_data(txt)
+class ExecuteFileCommand(Command):
+    def run(self):
+        f = interface.get_file_name()
+        if f:
+            with open(f, 'r') as t:
+                txt = t.read()
+            values = self.parse_data(txt)
+            if not values:
+                interface.print_error("Invalid values")
+            else:
+                create_line(values)
+        else:
+            interface.print_error("File not\nselected")
+
+
+class ExecuteTextCommand(Command):
+    def run(self):
+        txt = interface.get_text().get("1.0", "end")
+        if len(txt) == 1:
+            interface.print_error("No data in\nthe textbox")
+            return
+        values = self.parse_data(txt)
         if not values:
             interface.print_error("Invalid values")
         else:
             create_line(values)
-    else:
-        interface.print_error("File not\nselected")
 
 
-def execute_textbox():
-    txt = interface.get_text().get("1.0", "end")
-    if len(txt) == 1:
-        interface.print_error("No data in\nthe textbox")
-        return
-    values = parse_data(txt)
-    if not values:
-        interface.print_error("Invalid values")
-    else:
-        create_line(values)
+class ExecuteDataCommand(Command):
+    def run(self):
+        entries = interface.get_entries()
+        data = dict()
+        i = 0
+        if not entries.check_values():
+            interface.print_error("Invalid values")
+            return
+        for entry in entries.get_entries().values():
+            data.update({i: {"x": float(entry.get("x").get()), "y": float(entry.get("y").get())}})
+            i += 1
+        create_line(data)
 
 
-def execute_entries():
-    entries = interface.get_entries()
-    data = dict()
-    i = 0
-    if not entries.check_values():
-        interface.print_error("Invalid values")
-        return
-    for entry in entries.get_entries().values():
-        data.update({i: {"x": float(entry.get("x").get()), "y": float(entry.get("y").get())}})
-        i += 1
-    create_line(data)
+class Calculator:
+    def __init__(self):
+        self.__m = 0
+        self.__b = 0
+
+    def make_function(self):
+        def func(x): return self.__m * x + self.__b
+
+        return func
+
+    def calculate(self, data):
+        sumx = 0
+        sumy = 0
+        sumx2 = 0
+        sumxy = 0
+
+        points = data
+        for entry in points.values():
+            x = float(entry.get("x"))
+            y = float(entry.get("y"))
+            sumx += x
+            sumy += y
+            sumx2 += x * x
+            sumxy += x * y
+        n = len(points)
+        try:
+            m = (n * sumxy - sumx * sumy) / (n * sumx2 - sumx * sumx)
+            b = (sumy - m * sumx) / n
+        except ZeroDivisionError:
+            return False
+        self.__m = m
+        self.__b = b
+
+    def parse_points(self, func, data):
+        xs = [float(i.get("x")) for i in data.values()]
+        ys = [float(i.get("y")) for i in data.values()]
+        yfs = [func(x) for x in xs]
+        return xs, ys, yfs
 
 
-def execute():
-    interface.hide_error()
-    val = interface.get_var().get()
-    if val == 0:
-        interface.print_error("Method not\nselected")
-    elif val == 1:
-        execute_file()
-    elif val == 2:
-        execute_textbox()
-    elif val == 3:
-        execute_entries()
+class Plot:
+    def __init__(self):
+        self.__m = 0
+        self.__b = 0
+
+    def create_plot(self, xs, ys, yfs):
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.plot(xs, yfs, color='red', label='Function')
+        plt.scatter(xs, ys)
+        labels = [f"({x},{y})" for x, y in zip(xs, ys)]
+        for label, x, y in zip(labels, xs, ys):
+            plt.annotate(label, xy=(x, y), xytext=(5, -5), textcoords='offset points')
+        for x, y, yf in zip(xs, ys, yfs):
+            label = str(abs(y - yf))
+            plt.plot([x, x], [y, yf], color='black', linestyle='dashed', label=label)
+        plt.plot(xs, ys, color="blue")
+        mx = max(max(xs), max(ys), max(yfs))
+        mn = min(min(xs), min(ys), min(yfs))
+        plt.xlim([mn - 0.5, mx + 0.5])
+        plt.ylim([mn - 0.5, mx + 0.5])
+
+    def create_line(self, data):
+        x = calculate(data)
+        if not x:
+            interface.print_error("Invalid data")
+            return
+        else:
+            m, b = x
+        func = make_function(m, b)
+        xs, ys, yfs = parse_points(func, data)
+        plt.clf()
+        plt.title("Function: (" + str(m) + ") * x + (" + str(b) + ")")
+        self.create_plot(xs, ys, yfs)
+        plt.show()
+
+
 
 
 class Entries:
@@ -226,7 +234,7 @@ class Interface:
             return var
 
         def build_file(frm):
-            ttk.Button(frm, text="Upload file", command=openfile).grid(column=0, row=3)
+            ttk.Button(frm, text="Upload file", command=self.openfile).grid(column=0, row=3)
             file_label = ttk.Label(frm, text="File uploaded")
             return file_label
 
@@ -285,6 +293,17 @@ class Interface:
 
     def hide_error(self):
         self.__error.grid_forget()
+
+    def openfile(self):
+        filetypes = (
+            ('text files', '*.txt'),
+            ('All files', '*.*')
+        )
+        self.__file_label.grid_forget()
+        self.set_file_name(fd.askopenfilename(filetypes=filetypes))
+        if not self.__file_name:
+            self.__file_label.config(text="File not selected")
+        self.__file_label.grid(column=1, row=3)
 
     def show(self):
         self.__root.mainloop()
